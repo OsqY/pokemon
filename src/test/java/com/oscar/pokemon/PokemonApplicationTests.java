e6567a25043a97dfd3f5ab38a14aae2b83157b0f
OsqY <118946709+OsqY@users.noreply.github.com>
2023-10-23 09:49:30 -0600

    Creación de app

---
+++ src/test/java/com/oscar/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscar.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
